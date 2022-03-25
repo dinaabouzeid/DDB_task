68ec48c38aff6e86018fae3f6961c235013111d1
dinaabouzeid <61251940+dinaabouzeid@users.noreply.github.com>
2022-03-25 19:39:38 +0200

    Create testdb.sql

---
+++ testdb.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM ddb_task_DB.test
+LIMIT 0, 1000
+
+-- Date: 2022-03-25 19:38
+*/
+INSERT INTO `` (`id`,`name`) VALUES (1,'dina');
+INSERT INTO `` (`id`,`name`) VALUES (2,'ali');
+INSERT INTO `` (`id`,`name`) VALUES (3,'salwa');
+INSERT INTO `` (`id`,`name`) VALUES (4,'mohamed');
